4088f49eb18c13e6996deb81b583acd72be4e1e9
Rotii <niwatchai.wang@mail.kmutt.ac.th>
2024-03-12 11:49:42 +0700

    [UPDATE] Model Version 5

--- _preproText.py
+++ _preproText.py
@@ -27,7 +27,7 @@ def removeWord(resumeText):
     resumeText = re.sub('RT|cc', ' ', resumeText)  # remove RT and cc
     resumeText = re.sub('#\S+', '', resumeText)  # remove hashtags
     resumeText = re.sub('@\S+', '  ', resumeText)  # remove mentions
-    resumeText = re.sub('[%s]' % re.escape("""!"#$%&'()+,-./:;<=>?@[]^_`{|}~"""), ' ', resumeText)  # remove punctuations
+    resumeText = re.sub('[%s]' % re.escape("""%'()!"#$&;<=>?@[]^_`{|}-~"""), ' ',resumeText)
     resumeText = re.sub(r'[^\x00-\x7f]',r' ', resumeText)
     resumeText = re.sub('\s+', ' ', resumeText)  # remove extra whitespace
     return resumeText
